8dd18e840b6e7a6a2679430576c191827f79cda2
liyefei737 <macrofly@gmail.com>
2019-12-25 17:28:15 -0800

    slice.go is some code using slice

---
+++ src/slice.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+  "strconv"
+	"sort"
+)
+
+func main() {
+
+  var inputStr string
+  sli := make([]int, 0, 3) // create a slice of length 3
+
+  fmt.Printf("Enter an integer\n")
+  fmt.Scanf("%s", &inputStr)
+
+  for inputStr != "x" {
+    i, err := strconv.Atoi(inputStr)
+    if err != nil {
+      fmt.Println("a value that is not an integer nor x is entered. Please enter a valid integer.")
+    } else {
+      sli = append(sli, i)
+      sort.Ints(sli)
+      fmt.Println(sli)
+    }
+
+    fmt.Printf("Enter an integer\n")
+    fmt.Scanf("%s", &inputStr)
+  }
+}
