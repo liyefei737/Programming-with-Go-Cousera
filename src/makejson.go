970f3f0b3c6a2d0b76db3b6ee245216c81457207
liyefei737 <macrofly@gmail.com>
2019-12-26 12:30:30 -0800

    makejson converts a map into a json string

---
+++ src/makejson.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "encoding/json"
+	"fmt"
+  "os"
+  "bufio"
+)
+
+func main() {
+
+  var name string
+  var addr string
+
+  scanner := bufio.NewScanner(os.Stdin)
+
+  fmt.Println("Enter an name")
+  scanner.Scan()
+  name = scanner.Text()
+  fmt.Println("Enter an address")
+  scanner.Scan()
+  addr = scanner.Text()
+
+  var userInfo map[string]string
+  userInfo = make(map[string]string)
+
+  userInfo["name"] = name
+  userInfo["address"] = addr
+  jsonUserInfo, err := json.Marshal(userInfo)
+  if err != nil {
+    fmt.Println("json Marshaling failed")
+    return
+  }
+
+  fmt.Printf("%s\n", jsonUserInfo)
+
+}
